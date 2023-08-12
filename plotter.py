8e3699b34161246fe1c58de1165c56da6f9ceeeb
Adam Fu <afu082@aucklanduni.ac.nz>
2023-08-13 02:10:02 +1200

    new plot with inner lens

--- plotter.py
+++ plotter.py
@@ -1,24 +1,70 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Create the plot
+
+array_angles = np.arange(-90, 91, 10)
+
+
+def formulate_equation(angle):
+    return np.tan(np.radians(angle))
+
+gradients = [formulate_equation(angle) for angle in array_angles]
+
+
+inner_lens = -5
+outer_lens = -7
+
+intercepts = [-5 / gradient for gradient in gradients]
+
+    
+# pair gradients with intercepts
+gradient_intercept_pairs_1 = list(zip(gradients, intercepts))
+
+
+
 plt.figure(figsize=(8, 6))
 
-# Range of angles in degrees (from -90 to 90)
-angles_deg = np.arange(-90, 91, 10)
+for i, pair in enumerate(gradient_intercept_pairs_1):
+    
+    # if pair[1] not null
+    if(pair[1] >-10 and pair[1] < 10):
+        print(pair[0], pair[1])
+    
+        if(pair[1] < 0):
+            x_vals = np.linspace(pair[1], 0, 400)
+            y_vals = pair[0] * x_vals
+            
+        elif(pair[1] > 0):
+            x_vals = np.linspace(0, pair[1], 400)
+            y_vals = pair[0] * x_vals
+            
+        plt.plot(x_vals, y_vals, color = 'blue')
+        
+    
+    if((pair[1] <-10) or (pair[1] > 10)):
+        print(pair[0], pair[1])
+    
+        if(pair[1] < 0):
+            x_vals = np.linspace(-100, 100, 400)
+            y_vals = pair[0] * x_vals
+            
+        elif(pair[1] > 0):
+            x_vals = np.linspace(-100, 100, 400)
+            y_vals = pair[0] * x_vals
+            
+        plt.plot(x_vals, y_vals, color = 'blue')
+        
 
-# Convert angles to radians
-angles_rad = np.radians(angles_deg)
+# add horizontal line at y = -5
+x_vals = np.linspace(-10, 10, 400)
+y_vals = -5 * np.ones(400)
 
-# Loop through each angle and plot the lines
-for angle_rad in angles_rad:
-    x_vals = np.linspace(-10, 10, 400)
-    y_vals = np.tan(angle_rad) * x_vals
-    plt.plot(x_vals, y_vals)
+plt.plot(x_vals, y_vals, color = 'red', label = 'y = -5')
 
-# Set plot limits
-plt.xlim(-10, 10)
-plt.ylim(-20, 0)
+
+plt.xlim(-20, 20)
+plt.ylim(-40, 0)
+
 
 # Add labels and title
 plt.xlabel('x')
