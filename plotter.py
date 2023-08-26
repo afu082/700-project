f9226c7abc0c049756667fb408ccdabe5e4c6201
Adam Fu <afu082@aucklanduni.ac.nz>
2023-08-27 00:07:32 +1200

    rename variables, redefine line equations

--- plotter.py
+++ plotter.py
@@ -3,101 +3,89 @@ import matplotlib.pyplot as plt
 from math import pi
 
 
-angles_incidence_degrees = np.arange(10, 171, 10)  # [0,10,20...180]
-# angles_incidence_degrees = [30,60]
+angles_before_incidence_degrees = np.arange(10, 171, 10)  # [0,10,20...180]
+# angles_incidence_degrees = [60]
 
+
+# lens equations y = -5 
 inner_lens = -5
 outer_lens = -10
 
+# NEW represent lens as equations 
+eq_inner = [0, 0, -5]
+eq_outer = [0, 0, -10]
 
 
 
-angles_incidence_radians = np.radians(angles_incidence_degrees)
-
-refractive_index_lens = 1.49 # acrylic
+# refractive indices
+refractive_index_lens = 1.49 
 refractive_index_air = 1.000293 # air
 
 
 # get gradients from angle
-def formulate_equation(angle):
+def angle_to_gradient(angle):
     return np.tan(np.radians(angle))
 
+# array of gradients (b)
+gradients_incidence = [angle_to_gradient(angle) for angle in angles_before_incidence_degrees]
 
-# get angles from gradients
-def convert_gradient_to_angle(gradient):
+# array of intercepts (c)
+intercepts_inner_lens = [inner_lens / gradient for gradient in gradients_incidence]
 
-    return np.degrees(np.arctan(gradient))
-
-
-
-gradients_incidence = [formulate_equation(angle) for angle in angles_incidence_degrees]
-
-
-
-intercepts_incidence = [inner_lens / gradient for gradient in gradients_incidence]
-
-# pair gradients with intercepts
-gradient_intercept_pairs_incidence = list(zip(gradients_incidence, intercepts_incidence))
+# NEW new equations in form y = ax^2 + bx + c [a,b,c]
+equations_before_lens = [(0, gradient, intercept) for gradient, intercept in zip(gradients_incidence, intercepts_inner_lens)]
 
 # plot lines before refraction
 plt.figure(figsize=(8, 6))
-for i, pair in enumerate(gradient_intercept_pairs_incidence):
+for equation in equations_before_lens:
     
     # if pair[1] not null
-    if(pair[1] >-10 and pair[1] < 10):
+    if(equation[2] >-10 and equation[2] < 10):
     
-        if(pair[1] < 0):
-            x_vals = np.linspace(pair[1], 0, 400)
-            y_vals = pair[0] * x_vals
+        if(equation[2] < 0):
+            x_vals = np.linspace(equation[2], 0, 400)
+            y_vals = equation[0] * x_vals**2 + equation[1] * x_vals
             
-        elif(pair[1] > 0):
-            x_vals = np.linspace(0, pair[1], 400)
-            y_vals = pair[0] * x_vals
+        elif(equation[2] > 0):
+            x_vals = np.linspace(0, equation[2], 400)
+            y_vals = equation[0] * x_vals**2 + equation[1] * x_vals
             
         plt.plot(x_vals, y_vals, color = 'blue')
         
     
-    if((pair[1] <-10) or (pair[1] > 10)):
+    if((equation[2] <-10) or (equation[2] > 10)):
     
-        if(pair[1] < 0):
+        if(equation[2] < 0):
             x_vals = np.linspace(-100, 100, 400)
-            y_vals = pair[0] * x_vals
+            y_vals = equation[0] * x_vals**2 + equation[1] * x_vals
             
-        elif(pair[1] > 0):
+        elif(equation[2] > 0):
             x_vals = np.linspace(-100, 100, 400)
-            y_vals = pair[0] * x_vals
+            y_vals = equation[0] * x_vals**2 + equation[1] * x_vals
             
         plt.plot(x_vals, y_vals, color = 'blue')
         
         
+# angles of light in lens [10,20,30,...]
+angles_in_lens_degrees = np.degrees(pi/2 - np.arcsin((refractive_index_air / refractive_index_lens) * np.cos( np.radians(angles_before_incidence_degrees))))
 
-angle_of_refraction_radians = pi/2 - np.arcsin((refractive_index_air / refractive_index_lens) * np.cos( angles_incidence_radians))
+# gradients of light in lens [a,b,c,...]
+gradients_in_lens = [angle_to_gradient(angle) for angle in angles_in_lens_degrees]
 
-angle_of_refraction_degrees = np.degrees(angle_of_refraction_radians)
+c_in_lens = [(inner_lens - pairs[0] * pairs[1]) for pairs in zip(gradients_in_lens, intercepts_inner_lens)]
 
+equations_in_lens = [(0, gradients_in_lens, c_in_lens) for gradients_in_lens, c_in_lens in zip(gradients_in_lens, c_in_lens)]
 
-gradients_refraction = [formulate_equation(angle) for angle in angle_of_refraction_degrees]
+intercepts_outer_lens = [((outer_lens - new_c_value ) / gradient) for new_c_value, gradient in zip(c_in_lens, gradients_in_lens)]
 
-
-pair = list(zip(gradients_refraction, intercepts_incidence))
-
-
-new_c_values = [(inner_lens - pairs[0] * pairs[1]) for pairs in pair]
-
-intercepts_incidence_outer = [((outer_lens - new_c_value ) / gradient) for new_c_value, gradient in zip(new_c_values, gradients_refraction)]
-
-
-gradient_intercept_pairs_refraction = list(zip(gradients_refraction, intercepts_incidence_outer, intercepts_incidence, new_c_values))
-
-
-for i, pair in enumerate(gradient_intercept_pairs_refraction):
-    if pair[1] > -10 and pair[1] < 10:
-        if pair[1] < 0:
-            x_vals = np.linspace(pair[1], pair[2], 400)
-            y_vals = pair[0] * x_vals + pair[3]
-        elif pair[1] > 0:
-            x_vals = np.linspace(pair[2], pair[1], 400)
-            y_vals = pair[0] * x_vals + pair[3]
+for vals in zip(gradients_in_lens, intercepts_outer_lens, intercepts_inner_lens, c_in_lens):
+    if vals[1] > -10 and vals[1] < 10:
+        if vals[1] < 0:
+            x_vals = np.linspace(vals[1], vals[2], 400)
+            y_vals = vals[0] * x_vals + vals[3]
+        elif vals[1] > 0:
+            x_vals = np.linspace(vals[2], vals[1], 400)
+            y_vals = vals[0] * x_vals + vals[3]
             
         plt.plot(x_vals, y_vals, color = 'blue')
 
@@ -121,27 +109,26 @@ plt.plot(x_vals, y_vals, color = 'green', label = 'outer part of lens')
 # need old equation, new equation of line, x val of intercept, y val of intercept (y = -10)
 
 
-angle_of_exit_radians = pi/2 - np.arcsin((refractive_index_lens / refractive_index_air ) * np.cos(angle_of_refraction_radians))
+angle_of_exit_degrees = np.degrees( pi/2 - np.arcsin((refractive_index_lens / refractive_index_air ) * np.cos(np.radians(angles_in_lens_degrees))))
 
-angle_of_exit_degrees = np.degrees(angle_of_exit_radians)
+gradients_after_lens = [angle_to_gradient(angle) for angle in angle_of_exit_degrees]
 
-gradients_refraction_exit = [formulate_equation(angle) for angle in angle_of_exit_degrees]
+gradient_intercept_with_lens_pairs = list(zip(gradients_after_lens, intercepts_outer_lens))
 
-pair = list(zip(gradients_refraction_exit, intercepts_incidence_outer))
+c_values_exit = [(outer_lens - pairs[0] * pairs[1]) for pairs in gradient_intercept_with_lens_pairs]
 
-c_values_exit = [(outer_lens - pairs[0] * pairs[1]) for pairs in pair]
+equations_exit = [(0, gradients_after_lens, c_values_exit) for gradients_after_lens, c_values_exit in zip(gradients_after_lens, c_values_exit)]
 
-eq_3 = list(zip(gradients_refraction_exit, c_values_exit, intercepts_incidence_outer))
 
-for i, pair in enumerate(eq_3):
-    if(pair[1] >-10 and pair[1] < 10):
+for vals in zip(gradients_after_lens, c_values_exit, intercepts_outer_lens):
+    if(vals[1] >-10 and vals[1] < 10):
 
-        if pair[2] < 0:
-            x_vals = np.linspace(-1000, pair[2], 400)
-            y_vals = pair[0] * x_vals + pair[1]
-        elif pair[2] > 0:
-            x_vals = np.linspace(pair[2], 1000, 400)
-            y_vals = pair[0] * x_vals + pair[1]
+        if vals[2] < 0:
+            x_vals = np.linspace(-1000, vals[2], 400)
+            y_vals = vals[0] * x_vals + vals[1]
+        elif vals[2] > 0:
+            x_vals = np.linspace(vals[2], 1000, 400)
+            y_vals = vals[0] * x_vals + vals[1]
             
         plt.plot(x_vals, y_vals, color = 'blue')
 
