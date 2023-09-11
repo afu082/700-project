d7ae6bd3403824dc195c4a5347357c0572189a5b
Adam Fu <afu082@aucklanduni.ac.nz>
2023-09-11 17:51:04 +1200

    modularised code and fixed edge cases, working for initial and middle rays for all lens shapes up to quadratics

--- plotter.py
+++ plotter.py
@@ -2,23 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
 from math import inf
+import copy 
 
 
-def derivative_coefficients(coefficients):
+max_distance = 1000 
+
+
+def generate_angles(start, finish, step):
+    
+    angles_degrees = np.arange(start, finish + 1, step)
+    
+    angles_radians = np.radians(np.arange(start, finish + 1, step))
+    
+    return angles_degrees, angles_radians
+    
+    
+
+def find_derivative(coefficients):
     a, b, c = coefficients
     derivative_a = 2 * a
     derivative_b = b
     derivative_c = c*0
     return [derivative_a, derivative_b, derivative_c]
 
-# get gradients from angle
-def angle_to_gradient(angle):
-    return np.tan(np.radians(angle))
+
 
 def gradient_to_angle(gradient):
-    return np.degrees(np.arctan(gradient))
+    
+    angles_radians = np.arctan(gradient)
+    
+    return angles_radians
 
-# solve quadratic equation
+
+def angle_to_gradient(angle_in_degrees):
+    
+    gradients = np.tan(np.radians(angle_in_degrees))
+    
+    return gradients
+
+
+
 def solve_quadratic(a, b, c):
     discriminant = b**2 - 4 * a * c
     if discriminant < 0:
@@ -28,208 +51,215 @@ def solve_quadratic(a, b, c):
     else:
         return (-b + np.sqrt(discriminant)) / (2 * a), (-b - np.sqrt(discriminant)) / (2 * a)
     
+
+# using intial angle, find the equations of the light rays and their point of intersection with the lens
+# returns a list of ray equations in the form [m, c, start, finish, diffract] where m is gradient, c is y intercept, and start and finish are the x ranges and diffract is a boolean which is true if the ray diffracts and false if it does not
+# lenses are in the form [a,b,c,d,e,f,g] where a,b,c,d,e are the coefficients of the equation ax^2 + bx + c = dy^2 + ey and f and g are the x ranges of the lens
+def find_initial_ray_equations(angles_degrees, lens_equation):        
     
-# # Example coefficients: [a, b, c, d, e]
-# coefficients = [1, 0, -25, 1, 0,-10,10]  # Adjust these coefficients as needed
+    ray_equations = []
+    for angle in angles_degrees:
+        ray_equations.append([angle_to_gradient(angle), 0, 0, 0, True])
+        
+    x_intercept = []
 
-# A, B, C, D, E, START, END = coefficients
-
-# # Generate x and y values
-# x = np.linspace(START, END, 400)
-# y = np.linspace(-20, 20, 400)
-# X, Y = np.meshgrid(x, y)
-
-# # Evaluate the ellipse equation for each (x, y) pair
-# Z = A * X**2 + B * X + C + D * Y**2 - E * Y 
-
-# # Create the plot
-# plt.figure(figsize=(6, 6))
-# plt.xlim(-20, 20)
-# plt.ylim(-40, 0)
-# plt.contour(X, Y, Z, levels=[0], colors='b')  # Plot the ellipse where Z = 0
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('Plot of an Ellipse')
-# plt.grid()
-# plt.axis('equal')
-
-
-
-    
-
-
-angles_before_incidence_degrees = np.arange(10, 171, 10)  # [0,10,20...180]
-# angles_incidence_degrees = [60]
-
-
-
-# represent lens as equations  y = ax^2 + bx + c 
-# [a,b,c, x_start, x_end]
-eq_lens_inner = [1/10, 0, -3, -5.5, 5.5]
-
-eq_lens_outer = [0, 0, -10, -10, 10]
-
-
-
-# refractive indices
-refractive_index_lens = 1.49 
-refractive_index_air = 1.000293 # air
-
-
-# array of gradients (b)
-gradients_incidence = [angle_to_gradient(angle) for angle in angles_before_incidence_degrees]
-
-# x value of where beam intercepts inner lens
-if eq_lens_inner[0] == 0 and eq_lens_inner[1] == 0:
-    x_intercepts_of_inner_lens = [eq_lens_inner[2] / gradient for gradient in gradients_incidence] 
-elif eq_lens_inner[0] == 0 and eq_lens_inner[1] != 0:
-    x_intercepts_of_inner_lens = [eq_lens_inner[2] / (gradient - eq_lens_inner[1]) for gradient in gradients_incidence]
-    # for i, intercepts in enumerate(x_intercepts_of_inner_lens):
-    #     if eq_lens_inner[1] * intercepts + eq_lens_inner[2] > 0:
-    #         x_intercepts_of_inner_lens[i] = None
-elif eq_lens_inner[0] != 0:
-    intercepts = []
-    x_intercepts_of_inner_lens = []
-    for gradient in gradients_incidence:
-        intercepts.append(solve_quadratic(eq_lens_inner[0], eq_lens_inner[1] - gradient, eq_lens_inner[2]))
-    for i, intercept in enumerate(intercepts):
-        if(gradients_incidence[i] < 0):
-            x_intercepts_of_inner_lens.append(intercept[0])
-        elif(gradients_incidence[i] > 0):
-            x_intercepts_of_inner_lens.append(intercept[1])
-        elif(gradients_incidence[i] == 0):
-            x_intercepts_of_inner_lens.append(intercept[0])
+    if lens_equation[0] == 0 and lens_equation[1] == 0:
+        x_intercept = [(lens_equation[2] - ray_equations[i][1]) / ray_equations[i][0] for i in range(len(ray_equations))]
+    elif lens_equation[0] == 0 and lens_equation[1] != 0:
+        x_intercept = [(lens_equation[2] - ray_equations[i][1]) / (ray_equations[i][0] - lens_equation[1]) for i in range(len(ray_equations))]
+    elif lens_equation[0] != 0:
+        for i in range(len(ray_equations)):
+            first_solution, second_solution = solve_quadratic(lens_equation[0], lens_equation[1] - ray_equations[i][0], lens_equation[2] - ray_equations[i][1])
+            abs_diff_first = abs(first_solution)
+            abs_diff_second = abs(second_solution)
+            if abs_diff_first < abs_diff_second:
+                x_intercept.append(first_solution)
+            else:
+                x_intercept.append(second_solution)
             
-
+    for i in range(len(ray_equations)):
+        
+        ray_equations[i][2] = min(x_intercept[i], 0)
+        ray_equations[i][3] = max(x_intercept[i], 0)
+        
+        if(ray_equations[i][2] < lens_equation[5] or ray_equations[i][2] > lens_equation[6]):
+            ray_equations[i][2] = -max_distance
+            ray_equations[i][4] = False
+            
+        if(ray_equations[i][3] < lens_equation[5] or ray_equations[i][3] > lens_equation[6]):
+            ray_equations[i][3] = max_distance
+            ray_equations[i][4] = False
+    
+    ray_equations_true = []
+    ray_equations_false = []
+    for ray in ray_equations:
+        if(ray[4] == True):
+            ray_equations_true.append(ray)
+        else:
+            ray_equations_false.append(ray)
         
 
-# gradient intercept pairs
-gradient_range_values = [(0, gradients_incidence, x_intercepts_of_inner_lens) for gradients_incidence, x_intercepts_of_inner_lens in zip(gradients_incidence, x_intercepts_of_inner_lens)]
+    return ray_equations, ray_equations_true, ray_equations_false
 
-# equations of lines before lens in form y = ax^2 + bx + c [a,b,c, x_start, x_end, valid]
-equations_before_lens = [
-    [0, gradients_incidence, 0, min(x_intercepts_of_inner_lens[i], 0), max(x_intercepts_of_inner_lens[i], 0), True]
-    for i, gradients_incidence in enumerate(gradients_incidence)
-]
+# keep all lines but leave limits untouched when not diffracting
+    
 
-# plot lines before refraction
+    
+# plots lens in red
+def plot_lens(lens_equation):
+    x_vals = np.linspace(lens_equation[5], lens_equation[6], 400)
+    y_vals = lens_equation[0] * x_vals**2 + lens_equation[1] * x_vals + lens_equation[2]
+    plt.plot(x_vals, y_vals, color = 'red')
+    
+# only plots rays that diffract
+def plot_initial_rays(ray_equations):
+    for i, equation in enumerate(ray_equations):
+        # if(equation[4] == True):
+            x_vals = np.linspace(equation[2], equation[3], 400)
+            y_vals = equation[0] * x_vals + equation[1] 
+            plt.plot(x_vals, y_vals, color = 'blue')
+
+
+
+
+def find_middle_ray_equations(initial_ray_equations, inner_lens_equation, outer_lens_equation, refractive_index_lens, refractive_index_air):
+    # [m, c, start, finish, diffract]
+    # [a,b,c,d,e,f,g] where a,b,c,d,e are the coefficients of the equation ax^2 + bx + c = dy^2 + ey and f and g are the x ranges of the lens
+    # new ray equations copy initial ray equations
+    middle_ray_equations = copy.deepcopy(initial_ray_equations)
+    initial_angles_in_radians = [gradient_to_angle(equation[0]) for equation in initial_ray_equations]
+    for i, angle in enumerate(initial_angles_in_radians):
+        if(angle < 0):
+            initial_angles_in_radians[i] = pi + angle
+    theta_3 = np.zeros(len(initial_angles_in_radians))
+    
+    
+    for i, initial_ray_equation in enumerate(initial_ray_equations):
+        # only for rays that diffract
+        if(initial_ray_equation[4] == True):
+            # find theta_3
+            a,b,c = find_derivative(inner_lens_equation[0:3])
+            
+            if(abs(initial_ray_equation[2]) > abs(initial_ray_equation[3])):
+                tangent_at_x = a * initial_ray_equation[2] + b
+            else:
+                tangent_at_x = a * initial_ray_equation[3] + b
+            theta_3[i] = gradient_to_angle(tangent_at_x)
+            
+            # new gradient of ray in lens
+            # new_ray_equations[i][0] = angle_to_gradient(pi/2 - (-theta_3[i] + np.arcsin((refractive_index_air / refractive_index_lens) * np.cos(- theta_3[i] - (initial_angles_in_radians[i])))))
+            middle_ray_equations[i][0] = angle_to_gradient(np.degrees(pi/2 - (np.arcsin((refractive_index_air / refractive_index_lens) * np.sin(pi/2 - theta_3[i] - initial_angles_in_radians[i])))+ theta_3[i]))
+            # add theta_3 to the equation to account for tangent
+
+
+
+            # new y intercept of ray in lens
+            # wrong
+            if(abs(initial_ray_equation[2]) > abs(initial_ray_equation[3])):
+                middle_ray_equations[i][1] = (initial_ray_equations[i][0] * initial_ray_equations[i][2] + initial_ray_equations[i][1]) - middle_ray_equations[i][0] * initial_ray_equations[i][2]
+            else:
+                middle_ray_equations[i][1] = (initial_ray_equations[i][0] * initial_ray_equation[3] + initial_ray_equations[i][1]) - middle_ray_equations[i][0] * initial_ray_equation[3]
+                
+                
+                
+                
+                
+                
+            # new x intercept of ray in lens
+            
+            intercepts_outer_lens = ((outer_lens_equation[2] - middle_ray_equations[i][1] ) / middle_ray_equations[i][0]) 
+            
+            # find intercept of inner lens
+            
+            if(abs(initial_ray_equation[2]) > abs(initial_ray_equation[3])):
+                intercepts_inner_lens = initial_ray_equation[2]
+            else:
+                intercepts_inner_lens = initial_ray_equation[3]
+            
+            # for rays that go left
+            if(initial_ray_equation[0] > 0):
+                if(intercepts_outer_lens < 0):
+                    if(intercepts_outer_lens < outer_lens_equation[5] or intercepts_outer_lens > outer_lens_equation[6]):
+                        middle_ray_equations[i][2] = -max_distance
+                        middle_ray_equations[i][3] = intercepts_inner_lens
+                        middle_ray_equations[i][4] = False
+                    else:
+                        middle_ray_equations[i][2] = intercepts_outer_lens
+                        middle_ray_equations[i][3] = intercepts_inner_lens
+                        
+                else:
+                    if(intercepts_outer_lens < outer_lens_equation[5] or intercepts_outer_lens > outer_lens_equation[6]):
+                        middle_ray_equations[i][2] = -max_distance
+                        middle_ray_equations[i][3] = intercepts_inner_lens
+                        middle_ray_equations[i][4] = False
+                    else:
+                        middle_ray_equations[i][2] = intercepts_inner_lens
+                        middle_ray_equations[i][3] = -max_distance
+                
+            else:
+                if(intercepts_outer_lens > 0):
+                    if(intercepts_outer_lens < outer_lens_equation[5] or intercepts_outer_lens > outer_lens_equation[6]):
+                        middle_ray_equations[i][2] = max_distance
+                        middle_ray_equations[i][3] = intercepts_inner_lens
+                        middle_ray_equations[i][4] = False
+                    else:
+                        middle_ray_equations[i][2] = intercepts_outer_lens
+                        middle_ray_equations[i][3] = intercepts_inner_lens
+                        
+                else:
+                    if(intercepts_outer_lens < outer_lens_equation[5] or intercepts_outer_lens > outer_lens_equation[6]):
+                        middle_ray_equations[i][2] = max_distance
+                        middle_ray_equations[i][3] = intercepts_inner_lens
+                        middle_ray_equations[i][4] = False
+                    else:
+                        middle_ray_equations[i][2] = intercepts_inner_lens
+                        middle_ray_equations[i][3] = max_distance
+                
+            
+            
+            
+            
+            
+            
+            
+
+    return middle_ray_equations
+            
+            
+    
+
+
+
+
+
+
+
 plt.figure(figsize=(8, 6))
 
-for i, equation in enumerate(equations_before_lens):
-    #vert not plotting
-    if(equation[3] > eq_lens_inner[3] and equation[4] < eq_lens_inner[4]):
-        x_vals = np.linspace(equation[3], equation[4], 400)
-        y_vals = equation[0] * x_vals**2 + equation[1] * x_vals + equation[2]
-
-        plt.plot(x_vals, y_vals, color = 'blue')
-        
-    else:
-        if(equation[3] < 0):
-            x_vals = np.linspace(-100, 0, 400)
-            y_vals = equation[0] * x_vals**2 + equation[1] * x_vals + equation[2]
-            
-        elif(equation[4] > 0):
-            x_vals = np.linspace(0, 100, 400)
-            y_vals = equation[0] * x_vals**2 + equation[1] * x_vals + equation[2]
-        equations_before_lens[i][5] = False
-        plt.plot(x_vals, y_vals, color = 'red')
 
 
+equation_inner_lens = [1/2, 0, -3, 0, 1, -10, 10]
+equation_outer_lens = [0, 0, -5, 0, 1, -15, 15]
+plot_lens(equation_inner_lens)
+initial_ray_equations = find_initial_ray_equations(generate_angles(10, 170, 10)[0], equation_inner_lens)[0]
+# initial_ray_equations = find_initial_ray_equations(generate_angles(50, 50, 10)[0], equation_inner_lens)[0]
 
+plot_initial_rays(initial_ray_equations)
 
+plot_lens(equation_outer_lens)
 
+middle_ray_equations = find_middle_ray_equations(initial_ray_equations, equation_inner_lens, equation_outer_lens, 1.5, 1)
 
-
-
-# get angles of lines from gradients in equations_before_lens if valid
-valid_angles_in_lens_degrees = []
-for i, equation in enumerate(equations_before_lens):
-    if(equation[5] == True):
-        valid_angles_in_lens_degrees.append(gradient_to_angle(equation[1]))
-        
-        
-        
-
-
-# angles of light in lens [10,20,30,...]
-angles_in_lens_degrees = np.degrees(pi/2 - np.arcsin((refractive_index_air / refractive_index_lens) * np.cos(np.radians(angles_before_incidence_degrees))))
-
-# gradients of light in lens [a,b,c,...]
-gradients_in_lens = [angle_to_gradient(angle) for angle in angles_in_lens_degrees]
-
-c_in_lens = [(eq_lens_inner[2] - pairs[0] * pairs[1]) for pairs in zip(gradients_in_lens, x_intercepts_of_inner_lens)]
-
-equations_in_lens = [(0, gradients_in_lens, c_in_lens) for gradients_in_lens, c_in_lens in zip(gradients_in_lens, c_in_lens)]
-
-intercepts_outer_lens = [((eq_lens_outer[2] - new_c_value ) / gradient) for new_c_value, gradient in zip(c_in_lens, gradients_in_lens)]
-
-for vals in zip(gradients_in_lens, intercepts_outer_lens, x_intercepts_of_inner_lens, c_in_lens):
-    # TODO find limits based on quadratic equation
-    if vals[1] > -10 and vals[1] < 10:
-        if vals[1] < 0:
-            x_vals = np.linspace(vals[1], vals[2], 400)
-            y_vals = vals[0] * x_vals + vals[3]
-        elif vals[1] > 0:
-            x_vals = np.linspace(vals[2], vals[1], 400)
-            y_vals = vals[0] * x_vals + vals[3]
-            
-        plt.plot(x_vals, y_vals, color = 'blue')
-
-
-        
-
-# plot inner lens
-x_vals = np.linspace(eq_lens_inner[3], eq_lens_inner[4], 400)
-y_vals = eq_lens_inner[0] * x_vals**2 + eq_lens_inner[1] * x_vals + eq_lens_inner[2]
-plt.plot(x_vals, y_vals, color = 'red', label = 'inner part of lens')
-
-# plot outer lens
-x_vals = np.linspace(-10, 10, 400)
-y_vals = eq_lens_outer[2] * np.ones(400)
-plt.plot(x_vals, y_vals, color = 'green', label = 'outer part of lens')
-
-
-
-
-# plot points after outer lens
-# need old equation, new equation of line, x val of intercept, y val of intercept (y = -10)
-
-
-angle_of_exit_degrees = np.degrees( pi/2 - np.arcsin((refractive_index_lens / refractive_index_air ) * np.cos(np.radians(angles_in_lens_degrees))))
-
-gradients_after_lens = [angle_to_gradient(angle) for angle in angle_of_exit_degrees]
-
-gradient_intercept_with_lens_pairs = list(zip(gradients_after_lens, intercepts_outer_lens))
-
-c_values_exit = [(eq_lens_outer[2] - pairs[0] * pairs[1]) for pairs in gradient_intercept_with_lens_pairs]
-
-equations_exit = [(0, gradients_after_lens, c_values_exit) for gradients_after_lens, c_values_exit in zip(gradients_after_lens, c_values_exit)]
-
-
-for vals in zip(gradients_after_lens, c_values_exit, intercepts_outer_lens):
-    if(vals[1] >-10 and vals[1] < 10):
-
-        if vals[2] < 0:
-            x_vals = np.linspace(-1000, vals[2], 400)
-            y_vals = vals[0] * x_vals + vals[1]
-        elif vals[2] > 0:
-            x_vals = np.linspace(vals[2], 1000, 400)
-            y_vals = vals[0] * x_vals + vals[1]
-            
-        plt.plot(x_vals, y_vals, color = 'blue')
-
-    
-
-
+plot_initial_rays(middle_ray_equations)
 
 
 
 
 plt.xlim(-20, 20)
 plt.ylim(-40, 0)
+    
 
-
-# Add labels and title
+    
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Lines at Different Angles')
@@ -237,8 +267,7 @@ plt.axhline(0, color='black', linewidth=0.5)
 plt.axvline(0, color='black', linewidth=0.5)
 plt.grid(True, linestyle='--', alpha=0.7)
 
-# Add legend
-plt.legend()
-
-# Show the plot
 plt.show()
+
+    
+    
